199d2c8baf268a206005190f73787852ff7cf633
RomanichGNUSMAS <romanepremyan112009@gmail.com>
2026-02-04 20:49:07 +0400

    updated

--- Sorts/quickSort.js
+++ Sorts/quickSort.js
@@ -1,10 +1,10 @@
-function partition(arr,l,r){
+function partition(arr,l,r,compareFn){
     const pivot = arr[l];
     
     let [i,j] = [l + 1,r];
     while(i <= j){
-        while(arr[i] <= pivot) i++;
-        while(arr[j] > pivot) j--;
+        while(compareFn(arr[i],pivot) <= 0) i++;
+        while(compareFn(arr[j],pivot) > 0) j--;
         if(i < j) {
             [arr[i],arr[j]] = [arr[j],arr[i]];
             i++;
@@ -15,13 +15,20 @@ function partition(arr,l,r){
     return j;
 }
 
-function quickSort(arr,l = 0, r = arr.length - 1){
+function quickSort(arr,l = 0, r = arr.length - 1,compareFn){
     if(l >= r) return;
-    const index = partition(arr,l,r);
-    quickSort(arr,l,index - 1);
-    quickSort(arr,index + 1,r);
+    compareFn = typeof compareFn === 'function' 
+        ? compareFn
+        : (a,b) => a - b;
+    const index = partition(arr,l,r,compareFn);
+    quickSort(arr,l,index - 1,compareFn);
+    quickSort(arr,index + 1,r,compareFn);
 }
 
 const arr = [1,4,2,3,6,5,3];
-quickSort(arr);
+quickSort(arr,(a,b) =>  a - b);
 console.log(arr);
+
+
+
+module.exports = quickSort;
